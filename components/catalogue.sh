57945d5c1c104c60a7c7c1bedaa5f1758902ed55
devopsmoon <md_syd01@yahoo.co.in>
2021-06-27 18:53:36 +0530

    Added  code  to the scripting2 for catalogue

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -16,5 +16,16 @@ HEAD "Downloading the source code from github"
 curl -s -L -o /tmp/catalogue.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/558568c8-174a-4076-af6c-51bf129e93bb/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 unzip /tmp/catalogue.zip && npm install
 STAT $?
+HEAD "Updating the systemd files"
+sed -i 's/localhost/172.31.11.66/' /home/roboshop/catalogue/server.js
+sed -i 's/MONGO_ENDPOINT/172.31.11.66/' /home/roboshop/catalogue/systemd.service
+STAT $?
+HEAD "moving the systemd files to systems"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/systemd/catalogue.service
+STAT $?
+HEAD "Starting the services"
+systemctl start catalogue && systemctl enable catalogue
+STAT $?
+
 
 
